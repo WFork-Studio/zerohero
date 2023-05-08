9610743b2c71bfdafb5c88c332bc013ca0036b83
LowKey-R <rifkyabdulhanan@gmail.com>
2023-05-08 15:43:26 +0700

    [APP] Fix next head

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -9,6 +10,8 @@ export default function Document() {
           rel="stylesheet"
           href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
         ></link>
+        <Script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
+        <Script src="/js/chatbox.js" />
       </Head>
       <body>
         <Main />
