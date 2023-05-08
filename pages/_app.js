5a8dd16d3c6982979bf6620b3e1384d1e18ccac0
mb-1208 <mbntngfadlillahh1212@gmail.com>
2023-05-09 00:55:00 +0700

    modified

--- pages/_app.js
+++ pages/_app.js
@@ -4,27 +4,32 @@ import Head from "next/head";
 import Chatbox from "../components/Chatbox";
 import * as React from "react";
 import Script from "next/script";
+import { WalletProvider } from '@suiet/wallet-kit';
+import '@suiet/wallet-kit/style.css';
+
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Zero Hero</title>
-        <meta name="description" content="Zero Hero Official Website" />
-        <link rel="icon" href="/favicon.ico" />
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
-        <script src="/js/chatbox.js" />
-        <link rel="stylesheet" href="/css/chatbox.css"></link>
-        <link
-          rel="stylesheet"
-          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-        ></link>
-      </Head>
-      <header>
-        <Navbar />
-      </header>
-      <Component {...pageProps} />
-      <Chatbox />
+      <WalletProvider>
+        <Head>
+          <title>Zero Hero</title>
+          <meta name="description" content="Zero Hero Official Website" />
+          <link rel="icon" href="/favicon.ico" />
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
+          <script src="/js/chatbox.js" />
+          <link rel="stylesheet" href="/css/chatbox.css"></link>
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+          ></link>
+        </Head>
+        <header>
+          <Navbar />
+        </header>
+        <Component {...pageProps} />
+        <Chatbox />
+      </WalletProvider>
     </>
   );
 }
