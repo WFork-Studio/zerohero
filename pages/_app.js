646a57ee7599f556dfa952c75031c43746d4eec6
mb-1208 <mbntngfadlillahh1212@gmail.com>
2023-05-30 11:30:03 +0700

    [General] Create global variable function

--- pages/_app.js
+++ pages/_app.js
@@ -7,6 +7,9 @@ import { WalletProvider } from "@suiet/wallet-kit";
 import "@suiet/wallet-kit/style.css";
 import 'react-toastify/dist/ReactToastify.css';
 import { appWithTranslation } from 'next-i18next'
+import { useWallet } from "@suiet/wallet-kit";
+import { useEffect, useState } from "react";
+import AppContext from "../utils/AppContext";
 // import fsPromises from "fs/promises";
 // import path from "path";
 
@@ -21,28 +24,46 @@ import { appWithTranslation } from 'next-i18next'
 // }
 
 function MyApp({ Component, pageProps }) {
+  const [walletData, setWalletData] = useState();
+  const wallet = useWallet();
+
+  useEffect(() => {
+    if (wallet?.connected) {
+
+    }
+  }, [wallet]);
+
   return (
     <>
       <WalletProvider>
-        <Head>
-          <title>Zero Hero</title>
-          <meta name="description" content="Zero Hero Official Website" />
-          <link rel="icon" href="/favicon.ico" />
-          {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
+        <AppContext.Provider
+        value={{
+          state: {
+            walletData
+          },
+          setWalletData
+        }}
+        >
+          <Head>
+            <title>Zero Hero</title>
+            <meta name="description" content="Zero Hero Official Website" />
+            <link rel="icon" href="/favicon.ico" />
+            {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js" />
           <script src="/js/chatbox.js" /> */}
-          {/* <link rel="stylesheet" href="/css/chatbox.css"></link> */}
-          <link
-            rel="stylesheet"
-            href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
-          ></link>
-          <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-        </Head>
-        <header>
-          <Navbar />
-        </header>
-        <Component {...pageProps} />
+            {/* <link rel="stylesheet" href="/css/chatbox.css"></link> */}
+            <link
+              rel="stylesheet"
+              href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            ></link>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
+          </Head>
+          <header>
+            <Navbar />
+          </header>
+          <Component {...pageProps} />
 
-        <Chatbox />
+          <Chatbox />
+        </AppContext.Provider>
       </WalletProvider>
     </>
   );
